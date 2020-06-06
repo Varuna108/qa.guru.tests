4a430ce253f6059dc6e658a958b11d9311baff66
Varuna108 <varuna108@yandex.ru>
2020-06-06 17:27:00 +0300

    new file for jenkins

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,27 +2,35 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import drivers.CustomWebDriver;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Browsers.CHROME;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static helpers.AttachmentsHelper.*;
 
 public class TestBase {
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-        System.setProperty("selenoid_url", "selenoid.autotests.cloud");
-        Configuration.remote = "http://" + System.getProperty("selenoid_url") + ":4444/wd/hub";
-        }
+//        System.setProperty("selenoid_url", "selenoid.autotests.cloud");
+//        Configuration.remote = "http://" + System.getProperty("selenoid_url") + ":4444/wd/hub";
+        Configuration.browser = CustomWebDriver.class.getName();
+    }
 
 
-//    @AfterEach
-//    public void closeBrowser(){
-//        closeWebDriver();
-//    }
+    @AfterEach
+    void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachBrowserConsoleLogs();
+        attachVideo();
+        closeWebDriver();
+    }
 }
 
